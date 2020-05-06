c0a04ffeb1d2ebabe652c38d99bc539cd4c000af
redjab <quocminhdo92@gmail.com>
2020-05-06 10:17:46 -0600

    add optional google cloud sdk & add auto-start

--- setup-scripts/install.sh
+++ setup-scripts/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-#Install docker
+install_gcp=$1;
+
+# Install docker
 apt-get -y install \
     apt-transport-https \
     ca-certificates \
@@ -15,6 +17,28 @@ add-apt-repository \
    $(lsb_release -cs) \
    stable"
 
-apt-get update
+apt-get update && apt-get -y install docker-ce docker-ce-cli containerd.io
 
-apt-get -y install docker-ce docker-ce-cli containerd.io
+# Install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+service docker start
+
+# Optional: Install Google Cloud SDK
+if [ -n "$install_gcp" ]
+then 
+    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+    apt-get -y install apt-transport-https ca-certificates gnupg
+
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+
+    apt-get update && apt-get -y install google-cloud-sdk
+
+    gcloud init --console-only
+    # Authorize using url from above command
+fi
+
+# gsutil cp [OBJECT_LOCATION] gs://unifi-minh
