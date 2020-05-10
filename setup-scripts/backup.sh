c59f32ea39cbded0e9b0446c69f71de9c3fad687
redjab <quocminhdo92@gmail.com>
2020-05-10 11:30:48 -0600

    add pihole to backup script

--- setup-scripts/backup.sh
+++ setup-scripts/backup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-gsutil -m rsync -r -x ".\git/" /persist/home-assistant gs://debian-home-server-backup/home-assistant/$(date +%Y%m%d%H%M%S)
-gsutil -m rsync -r -x ".\git/" /persist/unifi-controller/data/backup gs://debian-home-server-backup/unifi-controller/$(date +%Y%m%d%H%M%S)
+current_date=$(date +%Y%m%d%H%M%S);
+
+gsutil -m rsync -r -x ".\git/" /persist/home-assistant gs://debian-home-server-backup/home-assistant/$current_date
+gsutil -m rsync -r -x ".\git/" /persist/unifi-controller/data/backup gs://debian-home-server-backup/unifi-controller/$current_date
+gsutil -m rsync -r -x ".\git/" /persist/pihole gs://debian-home-server-backup/pihole/$current_date
