b2c7edf51024db958923a248ef578fc6a81c749c
redjab <quocminhdo92@gmail.com>
2020-05-09 21:59:33 -0600

    change backup for unifi

--- setup-scripts/backup.sh
+++ setup-scripts/backup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-gsutil -m rsync -r -x ".*/.\git/" /persist gs://debian-home-server-backup/$(date +%Y%m%d%H%M%S)
+gsutil -m rsync -r -x ".*/.\git/" /persist/home-assistant gs://debian-home-server-backup/home-assistant/$(date +%Y%m%d%H%M%S)
+gsutil -m rsync -r -x ".*/.\git/" /persist/unifi-controller/data/backup gs://debian-home-server-backup/unifi-controller/$(date +%Y%m%d%H%M%S)
