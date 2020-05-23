94e4e7efd4e303efc14d244f7e02268641479488
redjab <quocminhdo92@gmail.com>
2020-05-23 09:54:19 -0600

    ignore database & log

--- setup-scripts/backup.sh
+++ setup-scripts/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gsutil -m rsync -r -x ".\git/" /persist/home-assistant gs://debian-home-server-backup/home-assistant/$(date +%Y-%m-%d-%H-%M-%S)
+gsutil -m rsync -r -x ".\git/|.*\.db$|.*\.log$" /persist/home-assistant gs://debian-home-server-backup/home-assistant/$(date +%Y-%m-%d-%H-%M-%S)
 gsutil -m rsync -r -x ".\git/" /persist/unifi-controller/data/backup gs://debian-home-server-backup/unifi-controller/$(date +%Y-%m-%d-%H-%M-%S)
