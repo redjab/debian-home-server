51a9c16cbff4a513fc103bb0c0ff0b0308d99515
redjab <quocminhdo92@gmail.com>
2020-05-23 21:55:38 -0600

    back up nginx

--- setup-scripts/backup.sh
+++ setup-scripts/backup.sh
@@ -2,3 +2,4 @@
 
 gsutil -m rsync -r -x ".\git/|.*\.db$|.*\.log$" /persist/home-assistant gs://debian-home-server-backup/home-assistant/$(date +%Y-%m-%d-%H-%M-%S)
 gsutil -m rsync -r -x ".\git/" /persist/unifi-controller/data/backup gs://debian-home-server-backup/unifi-controller/$(date +%Y-%m-%d-%H-%M-%S)
+gsutil -m rsync -r /persist/letsencrypt/nginx/site-confs gs://debian-home-server-backup/nginx/$(date +%Y-%m-%d-%H-%M-%S)
