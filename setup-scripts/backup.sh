c150c1669af6c47baca0a6729ef212ea10e58ce3
redjab <quocminhdo92@gmail.com>
2022-07-09 00:28:00 -0600

    epshome

--- setup-scripts/backup.sh
+++ setup-scripts/backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+find /persist/home-assistant/backups -maxdepth 1 -type f | xargs -x -r ls -t | awk 'NR>1' | xargs -r -L1 rm
 
-gsutil -m rsync -r -x ".*(\/)?\.git\/.*|.*\.db$|.*\.log$|core" /persist/home-assistant gs://debian-home-server.appspot.com/home-assistant/$(date +%Y-%m-%d-%H-%M-%S)
+gsutil -m rsync -r -x ".*(\/)?\.git\/.*|.*\.db$|.*\.log$|core" /persist/home-assistant/backups gs://debian-home-server.appspot.com/home-assistant/$(date +%Y-%m-%d-%H-%M-%S)
 gsutil -m rsync -r -x ".*(\/)?\.git\/.*" /persist/unifi-controller/data/backup gs://debian-home-server.appspot.com/unifi-controller/$(date +%Y-%m-%d-%H-%M-%S)
 gsutil -m rsync -r /persist/letsencrypt/nginx/site-confs gs://debian-home-server.appspot.com/nginx/$(date +%Y-%m-%d-%H-%M-%S)
 gsutil -m rsync -r -x ".*(\/)?\.git\/.*|\.npm\/.*|node_modules/|lib/|.*\.backup$|.*\.log$" /persist/node-red gs://debian-home-server.appspot.com/node-red/$(date +%Y-%m-%d-%H-%M-%S)
